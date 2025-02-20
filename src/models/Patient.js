d46791b1e8f5bbba015ee6cdbaadb7fc46be755e
isadoraAlexande <isadoraalexandre489@gmail.com>
2025-02-20 13:39:31 -0300

    adds models

---
+++ src/models/Patient.js
@@ -0,0 +1,29 @@
+class Patient {
+    constructor(name, birthDate, cpf, gender, city, status){
+        this.name = name;
+        this.birthDate = birthDate;
+        this.cpf = cpf;
+        this.gender = gender;
+        this.city = city;
+        this.status = status;
+    }
+
+    getInfo(){
+        return {
+            name: this.name,
+            birthDate: this.birthDate,
+            cpf: this.cpf,
+            gender: this.gender,
+            city: this.city,
+            status: this.status
+        };
+    }
+
+    changeStatus(newStatus){
+        this.status = newStatus;
+    }
+
+
+}
+
+module.exports = Patient;
