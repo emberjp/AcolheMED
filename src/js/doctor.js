50de9c63b8cc72eae3d57f6ccd035ebf201d36ea
isadoraAlexande <isadoraalexandre489@gmail.com>
2025-02-20 10:23:58 -0300

    things working better

--- src/js/doctor.js
+++ src/js/doctor.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Evento para desconectar
     disconnectBtn.addEventListener("click", function () {
         if (confirm("Deseja realmente sair?")) {
-            window.location.href = "login.html"; // Redireciona para login
+            window.location.href = "login.html"; 
         }
     });
 
@@ -21,7 +21,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // Evento para próximo paciente
     nextPatientBtn.addEventListener("click", function () {
         alert("Chamando próximo paciente...");
-        cleanPatientData();
+        //cleanPatientData();
+        preencherDados();
     });
 
     cancelAppointmentBtn.addEventListener("click", function() {
@@ -31,8 +32,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     startAppointmentBtn.addEventListener("click", function() {
-        loadPatientData();
+        
     });
 });
 
-function 
+function preencherDados() {
+    document.getElementById("Queixa").textContent = "Dor de cabeça ideswdfwantensa"; 
+    document.getElementById("Obsrvações").textContent = "Paciente relatou início há 3 dias";
+    document.getElementById("Medicamento").innerHTML = "Paracetamol 750mg";
+
+    document.getElementById("Alergia").textContent = "Nenhuma conhecida";
+    document.getElementById("Dor").textContent = "7";
+
+    document.getElementById("Temp").textContent = "37.5°C";
+    document.getElementById("Arterial").textContent = "120/80";
+    document.getElementById("Cardiaca").textContent = "75 bpm";
+    document.getElementById("Respiratória").textContent = "18 rpm";
+    document.getElementById("Peso").textContent = "70 kg";
+
+    document.getElementById("especificidade").textContent = "Exemplo";
+    document.getElementById("Es1").textContent = "100";
+    document.getElementById("Es2").textContent = "200";
+    document.getElementById("Es3").textContent = "300";
+    document.getElementById("Es4").textContent = "400";
+}
